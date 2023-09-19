101f0f4f552f88be535ab7869c011837357dc16c
devlopersabbir <devlopersabbir@gmail.com>
2023-09-20 03:39:07 +0600

    init generator

--- random_string_with_condition/GenerateString.java
+++ random_string_with_condition/GenerateString.java
@@ -1,5 +1,19 @@
+import java.util.Random;
+
 class GenerateString {
     public static void main(String[] args){
-        System.out.println("hello");
+        generator(10);
+    }
+
+    // generator method
+    static void generator(int length){
+        String numbers = "1234567890";
+        String spacialChar = "~!@#$%^&*()_-+=\\|]}{[?/><";
+        String alphabet = "abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ";
+
+        // make a single string using concat
+        String allCharacters = numbers + spacialChar + alphabet;
+
+
     }
 }
