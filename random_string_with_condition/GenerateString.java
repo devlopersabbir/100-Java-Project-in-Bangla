628ebb58225fa9f5d0873249ed29468909582ef0
devlopersabbir <devlopersabbir@gmail.com>
2023-09-20 13:33:18 +0600

    Password Cracker finished

--- random_string_with_condition/GenerateString.java
+++ random_string_with_condition/GenerateString.java
@@ -1,34 +1,56 @@
 import java.util.Random;
-import java.util.concurrent.Executors;
-import java.util.concurrent.ScheduledExecutorService;
-import java.util.concurrent.TimeUnit;
+import java.util.Timer;
+import java.util.TimerTask;
 
 class GenerateString {
+    static String password = "2022";
+    static int length = 4;
+    static int wait = 50;
+
     public static void main(String[] args) {
-        generator(4);
+        Timer timer = new Timer();
+        timer.schedule(new GeneratrTask(), 0, wait); // run every second
     }
 
     // generator method
-    static void generator(int length) {
-        String numbers = "1234567890";
-        // String spacialChar = "~!@#$%^&*()_-+=\\|]}{[?/><";
-        String alphabet = "abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ";
+    static class GeneratrTask extends TimerTask {
+        public void run() {
+            String generatedPass = generatorLoop(length);
 
+            if (generatedPass == password) {
+                System.out.format("Password matched! %s == %s \n", password, generatedPass);
+                System.exit(0);
+            } else {
+                System.out.format("Trying running -> %s != %s \n", password, generatedPass);
+            }
+        }
+    }
+
+    static void generator(int numberOfTimes, int length, String password) {
+
+    }
+
+    // running loop
+    static String generatorLoop(int length) {
+        final String numbers = "1234567890";
+        // String spacialChar = "~!@#$%^&*()_-+=\\|]}{[?/><";
+        // final String alphabet =
+        // "abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ";
         // make a single string using concat
-        String allCharacters = numbers + alphabet;
+        String allCharacters = numbers;
 
         // creating an object of random class
         Random random = new Random();
 
         // Create a StringBuilder to build the random string
         StringBuilder randomString = new StringBuilder();
-
         // Generate the random string
         for (int i = 0; i < length; i++) {
             int randomIndex = random.nextInt(allCharacters.length());
             char randomChar = allCharacters.charAt(randomIndex);
             randomString.append(randomChar);
         }
-        System.out.println(randomString.toString());
+
+        return randomString.toString();
     }
 }
