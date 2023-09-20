98272dad6aec6bdcb66d54215340b127815eec8b
devlopersabbir <devlopersabbir@gmail.com>
2023-09-20 15:07:07 +0600

    init

---
+++ random_number_generator_002/IntegerGenerator.java
@@ -0,0 +1,9 @@
+import java.util.Random;
+
+public class IntegerGenerator {
+    public static void main(String[] args) {
+        Random random = new Random();
+
+        System.out.println(random.nextInt(10));
+    }
+}
