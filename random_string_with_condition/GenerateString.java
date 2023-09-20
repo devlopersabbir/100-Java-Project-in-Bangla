b57119c54883d2a35d3a10a4c8a7455de33b476c
devlopersabbir <devlopersabbir@gmail.com>
2023-09-20 13:03:34 +0600

    Generator

--- random_string_with_condition/GenerateString.java
+++ random_string_with_condition/GenerateString.java
@@ -1,19 +1,34 @@
 import java.util.Random;
+import java.util.concurrent.Executors;
+import java.util.concurrent.ScheduledExecutorService;
+import java.util.concurrent.TimeUnit;
 
 class GenerateString {
-    public static void main(String[] args){
-        generator(10);
+    public static void main(String[] args) {
+        generator(4);
     }
 
     // generator method
-    static void generator(int length){
+    static void generator(int length) {
         String numbers = "1234567890";
-        String spacialChar = "~!@#$%^&*()_-+=\\|]}{[?/><";
+        // String spacialChar = "~!@#$%^&*()_-+=\\|]}{[?/><";
         String alphabet = "abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ";
 
         // make a single string using concat
-        String allCharacters = numbers + spacialChar + alphabet;
+        String allCharacters = numbers + alphabet;
 
+        // creating an object of random class
+        Random random = new Random();
 
+        // Create a StringBuilder to build the random string
+        StringBuilder randomString = new StringBuilder();
+
+        // Generate the random string
+        for (int i = 0; i < length; i++) {
+            int randomIndex = random.nextInt(allCharacters.length());
+            char randomChar = allCharacters.charAt(randomIndex);
+            randomString.append(randomChar);
+        }
+        System.out.println(randomString.toString());
     }
 }
