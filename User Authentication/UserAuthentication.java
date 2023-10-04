76cc0ec8e59420b63cfbd0badeb71b7987bc48f1
Sabbir Hossain Shuvo <82939905+devlopersabbir@users.noreply.github.com>
2023-10-04 22:36:42 +0600

    delete

--- User Authentication/UserAuthentication.java
+++ User Authentication/UserAuthentication.java
@@ -1,7 +1,5 @@
 class UserAuthentication {
     public static void main(String[] args) {
         System.out.println("Hello World");
-        System.out.println("Hello World 2");
-        System.out.println("Hello World 3");
     }
 }
