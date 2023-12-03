25d501bcf72096b09c1d22d35ee28569901bbc45
devlopersabbir <devlopersabbir@gmail.com>
2023-12-04 01:45:56 +0600

    triangle

---
+++ collage_exam_program/LTriangle.java
@@ -0,0 +1,9 @@
+class LTrangle {
+    public static void main(String[] args) {
+        // 1/2 * base * height
+        int base = 10;
+        int height = 20;
+
+        System.out.println((base * height) / 2);
+    }
+}
