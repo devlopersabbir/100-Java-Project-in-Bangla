b8cc05c2a6f5f9ee12d8ce258f95f12ee6dc8fb3
Sabbir Hossain Shuvo <82939905+devlopersabbir@users.noreply.github.com>
2023-12-19 23:12:11 +0600

    [chore🎇] using multiple method to make simple

--- Invoice Generator/InvoiceGenerator.java
+++ Invoice Generator/InvoiceGenerator.java
@@ -1,13 +1,17 @@
-import java.util.ArrayList;
-import java.util.List;
 import java.util.Scanner;
+import java.util.List;
+import java.util.ArrayList;
+class Invoice {
 
-class InvoiceGenerator {
-    public static void main(String[] args){
+    List<String> items = new ArrayList<>();
+    List<Double> prices = new ArrayList<>();
+
+    public static void main(String[] args) {
+        Invoice invoice = new Invoice();
+        invoice.run();
+    }
+    public void run(){
         Scanner scan = new Scanner(System.in);
-
-        List<String> items = new ArrayList<String>();
-        List<Double> prices = new ArrayList<Double>();
 
         System.out.println("Invoice Generator");
         System.out.println("-----------------");
@@ -15,30 +19,33 @@ class InvoiceGenerator {
         while(true){
             System.out.print("Enter item name (or 'done' to finished): ");
             String itemName = scan.nextLine();
-
             if(itemName.equalsIgnoreCase("done")){
-                 break;
-//                System.exit(0);
+                break;
             }
-
             System.out.print("Enter item price: ");
-            double itemPrice = scan.nextDouble();
+            double price = scan.nextDouble();
             scan.nextLine();
 
-            items.add(itemName);
-            prices.add(itemPrice);
+            this.items.add(itemName);
+            this.prices.add(price);
         }
-
-        // calculate total cost
+        // calculate total
+        double total = this.getPrice(this.prices);
+        this.generate(total);
+    }
+    // get total price
+    double getPrice(List<Double> prices){
         double total = 0;
         for(double price : prices){
             total += price;
         }
-
-        // Generate and display the invoice
+        return total;
+    }
+    // generate and display our invoice
+    public void generate(double total){
         System.out.println("\nInvoice");
-        System.out.println("-------");
-        for(int i = 0; i < items.size(); i++){
+        System.out.println("---------");
+        for(int i = 0; i < this.items.size(); i++) {
             System.out.printf("%s: $%.2f\n", items.get(i), prices.get(i));
         }
         System.out.println("---------------");
